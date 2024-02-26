74edfa3d6d9b7d59ba57d2fd811471fd424115cc
Quasolaris <magelan@quasolaris.tech>
2024-02-26 21:12:49 +0100

    [ADD] - Add exampel with return code

--- Learning/Raspberry_PI_4/adddexample.s
+++ Learning/Raspberry_PI_4/adddexample.s
@@ -4,6 +4,21 @@ _start:
 	// multiply 4 by -1 witzh movn and then adding 1
 	// means: 0010 (little endian) -> movn -> 1101 
 	// 1101 add 0001 -> 11111111111111111111111111111110 = -2
-	movn w0, #16
-	add w0, w0, #1
+	movn 	w0, #16
+	add		w0, w0, #1
+
+	// add can take an operand2 like lsl
+	add 	w0, w0, w0, lsl 3
+	mov 	w0, w0
+	
+	// make program retun result as return code
+	// w0 is retun register
+	mov 	w0, #10
+	add 	w0, w0, #1
+
+	// service command code 93, move into x8
+	mov 	x8, #93
+	// call linux to terminate
+	svc		0
+
 
